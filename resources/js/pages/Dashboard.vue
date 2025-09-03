83108f354bc8bcd0b688cedef28711ad66fa4488
Marelle Palm <marelle.palm@gmail.com>
2025-09-03 16:37:09 +0300

    weather.api

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
-import { type BreadcrumbItem } from '@/types';
+import { WeatherData, type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
@@ -11,6 +11,10 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: dashboard().url,
     },
 ];
+
+defineProps<{
+    weather: WeatherData;
+}>();
 </script>
 
 <template>
@@ -20,7 +24,18 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                    <div class="flex h-full justify-between p-6">
+                        <img class="items-center" :src="`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`" alt="" />
+                        <div class="flex gap-0.5">
+                            <h1 class="italic text-lg">
+                                City: {{ weather.name }} <br>
+                                {{ weather.weather[0].description }} <br>
+                                Temperature: {{ weather.main.temp }} <br />
+                                Wind: {{ weather.wind.speed }} m/s <br />
+                                Humidity: {{ weather.main.humidity }}% <br>
+                            </h1>
+                        </div>
+                    </div>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
