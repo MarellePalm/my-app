aea4db14a3659f965d073b912f15bacfefb71581
Marelle Palm <marelle.palm@gmail.com>
2026-03-25 13:26:22 +0200

    fix map marker

--- resources/js/components/MapView.vue
+++ resources/js/components/MapView.vue
@@ -10,6 +10,15 @@ import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
 
+delete (L.Icon.Default.prototype as any)._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL('leaflet/dist/images/marker-icon-2x.png', import.meta.url).href,
+  iconUrl: new URL('leaflet/dist/images/marker-icon.png', import.meta.url).href,
+  shadowUrl: new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).href,
+})
+
+
 const mapEl = ref(null);
 const selectedLocation = ref<{ lat: number; lng: number } | null>(null);
 
